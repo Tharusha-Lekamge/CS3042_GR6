46ffe2e61d89e2afbeb9662e83dd4b7dc04288d8
NivinyaSamarutilake <nivinxs@gmail.com>
2022-06-26 17:18:17 +0530

    fixes in homepage activity

--- app/src/main/java/com/example/microbank/HomepageActivity.java
+++ app/src/main/java/com/example/microbank/HomepageActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import data.AccountDAO;
+import data.Model.Account;
 
 public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
@@ -15,11 +23,29 @@ public class HomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-
         Button logout = findViewById(R.id.logoutBtn);
         ImageButton withdrawImgBtn = findViewById(R.id.withdrawalImgBtn);
         ImageButton depositImgBtn = findViewById(R.id.depositImgBtn);
+        RecyclerView rv = findViewById(R.id.rv_accList);
 
+        // demo accounts
+        Account acc1 = new Account("100020003000", "User123", "ADULT",5000);
+        Account acc2 = new Account("400050006000", "User123", "JOINT",20000);
+        Account acc3 = new Account("700080009000", "User123", "TEEN",13000);
+
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(acc1);
+        accountList.add(acc2);
+        accountList.add(acc3);
+        AccDisplayAdapter arr_adp = new AccDisplayAdapter(this, accountList);
+
+        // below line is for setting a layout manager for our recycler view.
+        // here we are creating vertical list so we will provide orientation as vertical
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+        rv.setLayoutManager(linearLayoutManager);
+        rv.setAdapter(arr_adp);
 
         withdrawImgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +86,5 @@ public class HomepageActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DepositActivity.class);
         startActivity(intent);
     }
-
 
 }
