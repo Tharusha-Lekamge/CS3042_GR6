3a364be4ab737466b11c446a6b317a35f1447bd7
Tharusha Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2022-07-07 11:39:10 +0530

    Added FD routes

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const transactionRouter = require("./routes/transactionRoutes");
 const userRouter = require("./routes/userRoutes");
 const syncRouter = require("./routes/syncRoutes");
 const accountRouter = require("./routes/accountRoutes");
+const fdRouter = require("./routes/fdRoutes");
 
 app.use("/api/v1/transaction", transactionRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/sync", syncRouter);
 app.use("/api/v1/account", accountRouter);
+app.use("/api/v1/fd", fdRouter);
 
 module.exports = app;
---
+++ controllers/fdController.js
@@ -0,0 +1,74 @@
+const db = require("../models/supportFunctions/dbOperations");
+const tableName = "fixeddeposits";
+const tableCols = `accountNumber, customerID, amount, openingDate, planType, FD_ID`;
+
+exports.createFD = async (req, res) => {};
+
+exports.withdrawFD = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const account = getFdByID(id);
+    // Get the FD balance
+    const balance = account[0].amount;
+    const accNo = account[0].accountNumber;
+    // Add the balance to the account Number as a transaction
+
+    // Delete the FD record
+    var sqlStatement = `DELETE FROM ${tableName} WHERE FD_ID = ${id}`;
+    const deleteRes = await db.query(sqlStatement);
+
+    res.status(200).json({
+      status: "Successfully withdrawn",
+      data: {
+        deleted: deleteRes,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      data: err,
+    });
+  }
+};
+
+const getFdByID = async (id) => {
+  try {
+    const sqlStatement = `SELECT * FROM ${tableName} WHERE FD_ID = ${id}`;
+    return await db.query(sqlStatement);
+  } catch (err) {
+    return [];
+  }
+};
+
+const getFdByAccNo = async (accNo) => {
+  try {
+    const sqlStatement = `SELECT * FROM ${tableName} WHERE accountNumber = ${accNo}`;
+    return await db.query(sqlStatement);
+  } catch (err) {
+    return [];
+  }
+};
+
+exports.getFD = async (req, res) => {
+  try {
+    var fdList = [];
+    const id = req.query.id;
+    if (!id) {
+      const accNo = req.query.accNo;
+      fdList = getFdByAccNo(accNo);
+    }
+    fdList = getFdByID(id);
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        fd: fdList,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      data: err,
+    });
+  }
+};
